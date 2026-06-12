72d97e32755946f4cbdb48790406dd735bd72ece
Rami <138556019+rami242424@users.noreply.github.com>
2026-06-12 13:18:51 +0900

    all in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
-
+import type { IBoard } from "./types";
 
 const initialBoards:IBoard = {
   "TO DO": [{ id: 1, text: "영어단어 외우기" }],
   "DOING": [{ id: 2, text: "운동하기" }],
   "DONE": [{ id: 3, text: "밥먹기" }],
 };
+
+function App(){
+  return(
+    <>
+      {Object.keys(initialBoards).map((boardName) => (
+      <>
+        <h2>{boardName}</h2>
+        {initialBoards[boardName].map((cards) => (
+          <div key={cards.id}>{cards.text}</div>
+        ))}
+      </>
+      ))}
+    </>
+  );
+}
+
+export default App;
