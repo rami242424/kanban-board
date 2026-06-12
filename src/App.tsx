f5758c72365072fa7f48d0d246ece6f085774f76
Rami <138556019+rami242424@users.noreply.github.com>
2026-06-12 15:59:11 +0900

    드래그앤드롭 + Recoil 연결 (React 18로 호환성 해결)

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,18 @@
 import { DragDropContext } from "@hello-pangea/dnd";
 import Board from "./components/Board";
-import type { IBoard } from "./types";
+import { useRecoilState } from "recoil";
+import { boardsState } from "./atoms";
 
-const initialBoards:IBoard = {
-  "TO DO": [{ id: 1, text: "영어단어 외우기" }],
-  "DOING": [{ id: 2, text: "운동하기" }],
-  "DONE": [{ id: 3, text: "밥먹기" }],
-};
 
 function App(){
+  const [boards, setBoards] = useRecoilState(boardsState);
   const onDragEnd = () => {
     // 드래그가 끝났을 때 할 일
   }
   return(
     <DragDropContext onDragEnd={onDragEnd}>
-      {Object.keys(initialBoards).map((boardName) => (
-      <Board boardName={boardName} cards={initialBoards[boardName]}/>
+      {Object.keys(boards).map((boardName) => (
+      <Board boardName={boardName} cards={boards[boardName]}/>
       ))}
     </DragDropContext>
   );
