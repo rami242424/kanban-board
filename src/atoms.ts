52d358db99a19fc7f3fe8c4f04d42db1bfd2d5bc
Rami <138556019+rami242424@users.noreply.github.com>
2026-06-19 16:31:20 +0900

    atom 비우기

--- src/atoms.ts
+++ src/atoms.ts
@@ -7,16 +7,9 @@ const {persistAtom} = recoilPersist();
 export const boardsState = atom<IBoard>({
     key: "boardsState",
     default: {
-        "TO DO": [
-            { id: "1", text: "영어단어 외우기" },
-            { id: "2", text: "독서하기" },
-            { id: "3", text: "산책하기" },
-        ],
-        "DOING": [
-            { id: "4", text: "운동하기" },
-            { id: "5", text: "조깅하기"}
-        ],
-        "DONE": [{ id: "6", text: "밥먹기" }],
+        "TO DO": [],
+        "DOING": [],
+        "DONE": [],
     },
     effects_UNSTABLE: [persistAtom],
 });
